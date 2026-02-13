0bbfdca30d204822e3db6009c92de86f20d2420f
prajwalpatil-afk <prajwalpatil910@gmail.com>
2026-02-13 15:37:29 +0530

    Bronze Updated

---
+++ sql/broze.sql
@@ -0,0 +1,13 @@
+IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_cust_info;
+GO
+
+CREATE TABLE bronze.crm_cust_info (
+    cst_id              INT,
+    cst_key             NVARCHAR(50),
+    cst_firstname       NVARCHAR(50),
+    cst_lastname        NVARCHAR(50),
+    cst_marital_status  NVARCHAR(50),
+    cst_gndr            NVARCHAR(50),
+    cst_create_date     DATE
+);
